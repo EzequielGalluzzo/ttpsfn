4e23ce6b7f3e6f7bb7ad7d23226fab6708118132
galluzzo <ezegalluzzo@hotmail.com>
2023-12-22 14:05:03 -0300

    routes working and bootstrap imported

---
+++ ttpsFront/src/app/components/home/home.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { Router, ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.css'
+})
+export class HomeComponent {
+
+  constructor(
+    private router: Router,
+  ){ }
+
+  login(){
+    console.log('llegue')
+    this.router.navigate(['/login'])
+  }
+
+}
